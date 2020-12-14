346623b12fe324aa5508ab55e9d2a156cf6d83ce
inparia <inparia@live.ca>
2020-12-14 01:27:48 -0500

    Initial commit

---
+++ GAME3023-FinalExam/Assets/Weather/ThunderSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class ThunderSystem : MonoBehaviour
+{
+    public Light2D thunder;
+    private float timeRemaining, thunderFlicker;
+    public bool flashEnabled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randNum = Random.Range(1, 5);
+        timeRemaining = randNum;
+        thunderFlicker = 0.2f;
+        flashEnabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flashEnabled)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                if(thunderFlicker >0)
+                {
+                    thunder.gameObject.SetActive(true);
+                    thunderFlicker -= Time.deltaTime;
+                }
+                else
+                {
+                    int randNum = Random.Range(1, 5);
+                    thunder.gameObject.SetActive(false);
+                    thunderFlicker = 0.2f;
+                    timeRemaining = randNum;
+                }
+                
+            }
+        }
+        else
+        {
+            int randNum = Random.Range(1, 5);
+            thunder.gameObject.SetActive(false);
+            thunderFlicker = 0.2f;
+            timeRemaining = randNum;
+        }
+    }
+
+}
---
+++ GAME3023-FinalExam/Assets/Weather/WeatherSystem.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public enum Weather
+{
+    SUNNY,
+    OVERCAST,
+    RAINY,
+    THUNDERSTORM
+}
+public class WeatherSystem : MonoBehaviour
+{
+
+    public Weather weather;
+
+    public AudioClip[] audioClips;
+
+    [Header("Systems")]
+    public SoundSystem soundSystem;
+    public RainSystem rainSystem;
+    public ThunderSystem thunderSystem;
+
+    [Header("Weather Light")]
+    public Light2D globalLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weather = Weather.SUNNY;
+        soundSystem = FindObjectOfType<SoundSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(weather)
+        {
+            case Weather.SUNNY:
+                changeMusic(audioClips[0]);
+                rainSystem.StopRain();
+                thunderSystem.flashEnabled = false;
+                globalLight.color = new Color(210, 0, 0, 0.1f);
+                break;
+            case Weather.RAINY:
+                changeMusic(audioClips[1]);
+                rainSystem.PlayRain();
+                thunderSystem.flashEnabled = false;
+                globalLight.color = new Color(0, 0, 186, 0.1f);
+                break;
+            case Weather.THUNDERSTORM:
+                changeMusic(audioClips[2]);
+                rainSystem.PlayRain();
+                thunderSystem.flashEnabled = true;
+                globalLight.color = new Color(0, 0, 186, 0.1f);
+                break;
+            case Weather.OVERCAST:
+                soundSystem.StopBGM();
+                rainSystem.StopRain();
+                thunderSystem.flashEnabled = false;
+                globalLight.color = new Color(0, 0, 186, 0.1f);
+                break;
+
+        }
+    }
+
+    void changeMusic(AudioClip audioClip)
+    {
+        if (soundSystem.BGM.isPlaying && soundSystem.BGM.clip.name == audioClip.name)
+        {
+            return;
+        }
+        else
+        {
+            soundSystem.changeBGM(audioClip);
+        }
+    }
+}
